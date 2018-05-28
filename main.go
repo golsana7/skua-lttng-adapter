40d8a79ba1432b9a49f20883edf2b13794057a7f
Harshal Sheth <hsheth2@gmail.com>
2018-05-28 15:34:32 -0400

    Go imports format

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"bufio"
 	"encoding/binary"
 	"fmt"
 	"os"
@@ -11,7 +12,6 @@ import (
 	"time"
 	"unsafe"
 
-	"bufio"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
 	"github.com/uber/jaeger-client-go"
